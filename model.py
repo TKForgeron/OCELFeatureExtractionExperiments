a3b7c6629a4e0e64d317affad5f0d76f35537d56
TKForgeron <tkforgeron@gmail.com>
2023-02-22 13:10:44 +0100

    first GAT results

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ torch.manual_seed(42)
 import torch
 from torch.nn import Sequential, MSELoss
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GATConv
 
 
 class GCN(torch.nn.Module):
@@ -38,3 +38,34 @@ class GCN(torch.nn.Module):
         # x = tf.reshape(h, (int(h.shape[0] / 4), (4 * 24))) MAKE THIS IN PYTORCH?
         out = self.out(x)
         return out
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, num_node_features, num_hidden_features):
+        super().__init__()
+
+        self.gconv1 = GATConv(num_node_features, num_hidden_features)
+        self.gconv2 = GATConv(num_hidden_features, num_hidden_features)
+        self.out = Linear(num_hidden_features, 1)
+
+    def forward(self, x, edge_index):
+        # x, edge_index = data.x, data.edge_index
+
+        # First Message Passing Layer (Transformation)
+        x = self.gconv1(x, edge_index)
+        x = x.relu()
+        # x = F.dropout(x, p=0.5, training=self.training)
+
+        # Second Message Passing Layer
+        x = self.gconv2(x, edge_index)
+        x = x.relu()
+        # x = F.dropout(x, p=0.5, training=self.training)
+
+        # Output layer
+        # x = F.mse_loss()
+        # x = tf.reshape(h, (int(h.shape[0] / 4), (4 * 24))) MAKE THIS IN PYTORCH?
+        out = self.out(x)
+        return out
+
+    def get_class_name(self) -> None:
+        return str(self).split("(")[0]
