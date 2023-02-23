d36d5611a56f335734c1dbd5ec3b27926f9adce9
TKForgeron <trollololollolol.js@gmail.com>
2023-02-23 17:11:15 +0100

    add validation set splitting

--- ocpa/algo/predictive_monitoring/obj.py
+++ ocpa/algo/predictive_monitoring/obj.py
@@ -137,6 +137,7 @@ class Feature_Storage:
         self._scaler = scaler
         # self._graph_indices: list[int] = None
         self._training_indices = None
+        self._validation_indices = None
         self._test_indices = None
         self._target_label = None
 
@@ -167,11 +168,23 @@ class Feature_Storage:
     def _set_scaler(self, scaler):
         self._scaler = scaler
 
-    def _get_training_indices(self):
+    def _get_training_indices(self) -> list[int]:
         return self._training_indices
 
-    def _get_test_indices(self):
+    def _set_training_indices(self, training_indices):
+        self._training_indices = training_indices
+
+    def _get_validation_indices(self) -> list[int]:
+        return self._validation_indices
+
+    def _set_validation_indices(self, validation_indices):
+        self._validation_indices = validation_indices
+
+    def _get_test_indices(self) -> list[int]:
         return self._test_indices
+
+    def _set_test_indices(self, test_indices):
+        self._test_indices = test_indices
 
     def _get_target_label(self):
         return self._target_label
@@ -183,8 +196,9 @@ class Feature_Storage:
     execution_features = property(_get_execution_features, _set_execution_features)
     feature_graphs = property(_get_feature_graphs, _set_feature_graphs)
     scaler = property(_get_scaler, _set_scaler)
-    training_indices = property(_get_training_indices)
-    test_indices = property(_get_test_indices)
+    training_indices = property(_get_training_indices, _set_training_indices)
+    validation_indices = property(_get_validation_indices, _set_validation_indices)
+    test_indices = property(_get_test_indices, _set_test_indices)
     target_label = property(_get_target_label, _set_target_label)
 
     def _event_id_table(self, feature_graphs):
@@ -212,7 +226,11 @@ class Feature_Storage:
         return mapper
 
     def extract_normalized_train_test_split(
-        self, test_size: float, target_label: tuple, state: int = 42
+        self,
+        test_size: float,
+        validation_size: float = None,
+        target_label: tuple = None,
+        state: int = 42,
     ):
         """
         Splits and normalizes the feature storage. Each split is normalized according to it's member, i.e., the testing
@@ -221,21 +239,42 @@ class Feature_Storage:
         :param test_size: Between 0 and 1, indicates the share of the data that should go to the test set.
         :type test_size: float
 
+        :param validation_size: Between 0 and 1, indicates the share of the data (percentage points) that should go
+        to the validation set. It takes this from the training set size.
+        :type validation_size: float
+
         :param state: random state of the splitting. Can be used to reproduce splits
         :type state: int
 
 
         """
-
+        # Set train/val/test indices
+        train_size = 1 - validation_size - test_size
+        if validation_size >= train_size:
+            raise ValueError(
+                f"validation_size ({validation_size}) must be smaller than train_size (= 1-test_size = {train_size})"
+            )
         graph_indices = list(range(0, len(self.feature_graphs)))
         random.Random(state).shuffle(graph_indices)
-        split_index = int((1 - test_size) * len(graph_indices))
-        self._training_indices = graph_indices[:split_index]
-        self._test_indices = graph_indices[split_index:]
+        # @@@@@@@@@@@@@@@@@@ ######## &&&&&&&&&&&&
+        #       train          val        test
+        #         50           20          30
+        #                   v        v
+        #             train_spl_idx  val_spl_idx
+        train_split_idx = int(train_size * len(graph_indices))
+        val_split_idx = int((train_size + validation_size) * len(graph_indices))
+        self._set_training_indices(graph_indices[:train_split_idx])
+        self._set_validation_indices(graph_indices[train_split_idx:val_split_idx])
+        self._set_test_indices(graph_indices[val_split_idx:])
 
-        train_graphs, test_graphs = [
-            self.feature_graphs[i] for i in self._training_indices
-        ], [self.feature_graphs[i] for i in self._test_indices]
+        train_graphs, val_graphs, test_graphs = (
+            [self.feature_graphs[i] for i in self._training_indices],
+            [self.feature_graphs[i] for i in self._validation_indices],
+            [self.feature_graphs[i] for i in self._test_indices],
+        )
+
+        # TEST THIS, AND FURTHER IMPLEMENT VALIDATION SPLITTING!!!
+
         # Normalize (except for target variable)
         train_table = self._event_id_table(train_graphs)
         test_table = self._event_id_table(test_graphs)
